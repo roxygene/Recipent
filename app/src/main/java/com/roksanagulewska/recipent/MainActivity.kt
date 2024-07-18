561a5b403086553c3bb6565c8618f0f3e4156ccc
Roksana <rgulewska@gmail.com>
2024-07-18 21:06:41 +0200

    Almost fixed theming and started working on proper cards

--- app/src/main/java/com/roksanagulewska/recipent/MainActivity.kt
+++ app/src/main/java/com/roksanagulewska/recipent/MainActivity.kt
@@ -3,7 +3,9 @@ package com.roksanagulewska.recipent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,20 +23,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.roksanagulewska.recipent.ui.theme.RecipentTheme
 import com.roksanagulewska.recipent.Recipe
 import com.roksanagulewska.recipent.tags.RecipeCategory
+import androidx.compose.ui.unit.sp
+import com.roksanagulewska.recipent.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            
         }
     }
 }
@@ -41,26 +48,36 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun RecipeCard(painter: Painter, recipe: Recipe, modifier: Modifier = Modifier) {
     Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(10.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+        modifier = modifier
+            .fillMaxWidth()
+            .height(200.dp),
+            shape = RoundedCornerShape(15.dp),
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
+            border = BorderStroke(width = 2.dp, color = MaterialTheme.colorScheme.outline)
+
     ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = null,
-                contentScale = ContentScale.Crop
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
-            ) {
-                Text(recipe.name, )
-            }
+        Column(modifier.fillMaxSize()) {
 
         }
+
+        /*Box(modifier = Modifier.height(200.dp).background(Yellow)) {//to stack content (first element is on the bottom of stack)
+            Image( //dish pic
+                painter = painter,
+                contentDescription = "Picture of ${recipe.name}",
+                contentScale = ContentScale.Crop,
+                alignment = Alignment.TopCenter
+
+            )
+            *//*Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(15.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(recipe.name, style = TextStyle(color = Black, fontSize = 16.sp))
+            }*//*
+
+        }*/
     }
     //recipe name
     //picture (opt)
@@ -68,7 +85,7 @@ fun RecipeCard(painter: Painter, recipe: Recipe, modifier: Modifier = Modifier) 
     //preparation time (opt)
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = false)
 @Composable
 fun RecipeCardPreview() {
     val painter = painterResource(id = R.drawable.spaghetti_bolognese)
--- app/src/main/java/com/roksanagulewska/recipent/ui/theme/Color.kt
+++ app/src/main/java/com/roksanagulewska/recipent/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.roksanagulewska.recipent.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Orange1000 = Color(0xFF3D1B00)
 val Orange900 = Color(0xFFFF6D00)
 val Orange800 = Color(0xFFFF9100)
 val Orange700 = Color(0xFFFFA201)
@@ -13,6 +14,7 @@ val Orange200 = Color(0xFFFFDE82)
 val Orange100 = Color(0xFFFFEBB3)
 val Orange50 = Color(0xFFFFF7E1)
 
+val Purple1000 = Color(0xFF21194D)
 val Purple600 = Color(0xFF523DBD)
 val Purple500 = Color(0xFF5C42C3)
 val Purple400 = Color(0xFF755DCD)
@@ -23,10 +25,16 @@ val Purple50 = Color(0xFFECE8F7)
 
 val Yellow = Color(0xFFFFEC1F)
 val Pink = Color(0xFFF566B0)
-val Red = Color(0xFFEB0825)
 val Green = Color(0xFF19BE48)
 val Blue = Color(0xFF2CA4FF)
 
+
+val LightRed = Color(0xFFf7a1ab)
+val Red = Color(0xFFEB0825)
+val DarkRed = Color(0xFF620410)
+
+val Black = Color(0xFF000000)
+val Grey1000 = Color(0xFF1F1F1F)
 val Grey900 = Color(0xFF262626)
 val Grey800 = Color(0xFF484848)
 val Grey700 = Color(0xFF676767)
@@ -36,6 +44,4 @@ val Grey400 = Color(0xFFC3C3C3)
 val Grey300 = Color(0xFFE6E6E6)
 val Grey200 = Color(0xFFF1F1F1)
 val Grey100 = Color(0xFFF6F6F6)
-
-val Black = Color(0xFF000000)
 val White = Color(0xFFFFFFFF)
